af91cdd4f5ad99e8fd17f9ec7bcb8b7b91f0cfae
Danny <samboard@hotmail.com>
2024-05-19 11:22:37 -0400

    Adding dummy data

--- src/app.py
+++ src/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
@@ -13,7 +13,22 @@ def main():
      </form>
      '''
 
+headings = (
+    "Username", 
+    "Follower", 
+    "Following",
+    "Posts",
+    "Average likes in the last 9 posts",
+    "Most recent post date",
+    "Number of followers",
+    "Number of followings",
+    "Last updated")
+data = (
+    ("samboard", "Yes", "No", "796", "209", "Jan 11, 2022", "4627", "5059", "May 18, 2024"),
+    ("sk8mafia", "Yes", "Yes", "7641", "1033", "May 17, 2024", "429K", "1570", "May 17, 2024 "),
+    (".....", ".....", ".....", ".....", ".....", ".....", ".....", ".....", ".....")
+)
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
-    input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
+    user_input = request.form.get("user_input", "")
+    return render_template("table.html", headings=headings, data=data, user_input=user_input)
