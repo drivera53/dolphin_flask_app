d3ea22b930ce30f83a200e381014bba67ec8c160
Danny <samboard@hotmail.com>
2024-06-22 11:51:57 -0400

    Connecting with data_analyzer REST API

--- src/app.py
+++ src/app.py
@@ -1,35 +1,42 @@
 #!/usr/bin/env python3
 
 from flask import Flask, request, render_template
+from utils.req_data_analyzer import get_all_posts_metrics
 
 app = Flask(__name__)
 
 @app.route("/")
 def main():
     return '''
-        <h1>Enter your Instagram Username</h1>
-     <form action="/echo_user_input" method="POST">
-         <input name="user_input">
-         <input type="submit" value="Submit!">
-     </form>
+        <h1>Dolphin Instagram Metrics</h1>
+        <form action="/ig_metrics" method="post">
+            <button name="foo">See IG users data</button>
+        </form>
+        <form action="/ig_metrics" method="POST">
+            <input name="user_input">
+            <input type="submit" value="Add new IG username">
+        </form>
      '''
 
 headings = (
-    "Username", 
-    "Follower", 
-    "Following",
+    "User ID",
+    "Username",
+    "Full name", 
     "Posts",
     "Average likes in the last 9 posts",
     "Most recent post date",
-    "Number of followers",
-    "Number of followings",
     "Last updated")
-data = (
-    ("samboard", "Yes", "No", "796", "209", "Jan 11, 2022", "4627", "5059", "May 18, 2024"),
-    ("sk8mafia", "Yes", "Yes", "7641", "1033", "May 17, 2024", "429K", "1570", "May 17, 2024 "),
-    (".....", ".....", ".....", ".....", ".....", ".....", ".....", ".....", ".....")
-)
-@app.route("/echo_user_input", methods=["POST"])
-def echo_input():
-    user_input = request.form.get("user_input", "")
-    return render_template("table.html", headings=headings, data=data, user_input=user_input)
+# data = (
+#     ("samboard", "Yes", "No", "796", "209", "Jan 11, 2022", "4627", "5059", "May 18, 2024"),
+#     ("sk8mafia", "Yes", "Yes", "7641", "1033", "May 17, 2024", "429K", "1570", "May 17, 2024 "),
+#     (".....", ".....", ".....", ".....", ".....", ".....", ".....", ".....", ".....")
+# )
+@app.route("/ig_metrics", methods=["POST"])
+def ig_metrics():
+    # user_input = request.form.get("user_input", "")
+    user_input = "Danny" # Hardcoding name, name will be retrieved once adding auth0
+    data = get_all_posts_metrics()
+    return render_template("ig_metrics.html", headings=headings, data=data, user_input=user_input)
+
+if __name__ == '__main__': 
+    app.run(host='127.0.0.1', port=3000, debug=True)
---
+++ src/utils/req_data_analyzer.py
@@ -0,0 +1,12 @@
+import requests
+
+FAKE_API_KEY = "12345"
+DATA_ANALYZER_REST_API_IG_DATA = "http://127.0.0.1:5000/igdata/"
+
+def get_all_posts_metrics():
+    response = requests.get(DATA_ANALYZER_REST_API_IG_DATA + FAKE_API_KEY)
+    data = []
+    for user in response.json()["data"]:
+        curr_user = (user["id"], user["username"], user["full_name"], user["posts"], user["average_likes"], user["most_recent_post"], user["last_updated"])
+        data.append(curr_user)
+    return data
