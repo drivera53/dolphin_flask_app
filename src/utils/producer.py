20726a59d1a97078dd21cf330005e427cd7efd6c
Danny <samboard@hotmail.com>
2024-06-23 11:20:23 -0400

    Adding MQ producer

--- src/utils/producer.py
+++ src/utils/producer.py
@@ -2,21 +2,24 @@
 import json, pika
 
 def create_new_username_MQ(username):
-    '''
-    Establish a connection to a RabbitMQ server.
-    localhost means we are connecting to the local
-    machine. However we can provide a IP address to
-    a different machine.
-    '''
-    conn = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
-    channel = conn.channel()
+    try:
+        '''
+        Establish a connection to a RabbitMQ server.
+        localhost means we are connecting to the local
+        machine. However we can provide a IP address to
+        a different machine.
+        '''
+        conn = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+        channel = conn.channel()
 
-    '''
-    We create a queue just for new_ig_username
-    '''
-    channel.queue_declare(queue='new_ig_username')
+        '''
+        We create a queue just for new_ig_username
+        '''
+        channel.queue_declare(queue='new_ig_username')
 
-    channel.basic_publish(exchange="", routing_key='new_ig_username',
-                        body=json.dumps({
-                            "new_username": username
-                        }))
+        channel.basic_publish(exchange="", routing_key='new_ig_username',
+                            body=json.dumps({
+                                "new_username": username
+                            }))
+    except:
+        print("An exception occurred while adding new message to MQ: new_ig_username")
