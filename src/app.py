f56f40362229ede904e27fb4289f5cac0b9717a8
Danny <samboard@hotmail.com>
2024-06-23 11:00:08 -0400

    Adding MQ producer

--- src/app.py
+++ src/app.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, request, render_template
 from utils.req_data_analyzer import get_all_posts_metrics
+from utils.producer import create_new_username_MQ
 
 app = Flask(__name__)
 
@@ -9,34 +10,36 @@ app = Flask(__name__)
 def main():
     return '''
         <h1>Dolphin Instagram Metrics</h1>
-        <form action="/ig_metrics" method="post">
+        <form action="/ig_metrics" method="POST">
             <button name="foo">See IG users data</button>
         </form>
-        <form action="/ig_metrics" method="POST">
-            <input name="user_input">
+        <form action="/add_ig_username" method="POST">
+            <input name="ig_username">
             <input type="submit" value="Add new IG username">
         </form>
      '''
 
-headings = (
-    "User ID",
-    "Username",
-    "Full name", 
-    "Posts",
-    "Average likes in the last 9 posts",
-    "Most recent post date",
-    "Last updated")
-# data = (
-#     ("samboard", "Yes", "No", "796", "209", "Jan 11, 2022", "4627", "5059", "May 18, 2024"),
-#     ("sk8mafia", "Yes", "Yes", "7641", "1033", "May 17, 2024", "429K", "1570", "May 17, 2024 "),
-#     (".....", ".....", ".....", ".....", ".....", ".....", ".....", ".....", ".....")
-# )
 @app.route("/ig_metrics", methods=["POST"])
 def ig_metrics():
     # user_input = request.form.get("user_input", "")
+
     user_input = "Danny" # Hardcoding name, name will be retrieved once adding auth0
+    headings = (
+        "User ID",
+        "Username",
+        "Full name", 
+        "Posts",
+        "Average likes in the last 9 posts",
+        "Most recent post date",
+        "Last updated")
     data = get_all_posts_metrics()
     return render_template("ig_metrics.html", headings=headings, data=data, user_input=user_input)
+
+@app.route("/add_ig_username", methods=["POST"])
+def add_ig_username():
+    new_ig_username = request.form.get("ig_username", "")
+    create_new_username_MQ(new_ig_username)
+    return render_template("new_ig_username.html", ig_username=new_ig_username)
 
 if __name__ == '__main__': 
     app.run(host='127.0.0.1', port=3000, debug=True)
---
+++ src/utils/producer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+import json, pika
+
+def create_new_username_MQ(username):
+    '''
+    Establish a connection to a RabbitMQ server.
+    localhost means we are connecting to the local
+    machine. However we can provide a IP address to
+    a different machine.
+    '''
+    conn = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
+    channel = conn.channel()
+
+    '''
+    We create a queue just for new_ig_username
+    '''
+    channel.queue_declare(queue='new_ig_username')
+
+    channel.basic_publish(exchange="", routing_key='new_ig_username',
+                        body=json.dumps({
+                            "new_username": username
+                        }))
